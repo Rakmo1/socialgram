fa28fa81eac4736dc96ee44649dde3390187748f
omkar <omkarbhiwande@gmail.com>
2019-12-16 23:14:51 +0530

    Changed index file

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,22 @@
 const functions = require('firebase-functions');
+const app =require('express')();
+const FBAuth=require('./util/FBAuth');
+const {getPosts,createPost, test,getPost}=require('./handlers/posts');
+const {signup,login, uploadImage, updateUserDetails,getAuthenticatedUser}=require('./handlers/users');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+//Posts Path
+app.get('/getPosts',FBAuth,getPosts);
+app.post('/createPost',FBAuth,createPost);
+app.post('/test',test);
+app.get('/post/:postId',FBAuth,getPost);
+
+//Users Path
+app.post('/signup',signup);
+app.post('/login',login);
+app.post('/user/image',FBAuth,uploadImage);
+app.post('/userDetails',FBAuth,updateUserDetails);
+app.get('/getUser',FBAuth,getAuthenticatedUser);
+// app.post('/user/deleteImage',FBAuth,deleteUploadedImage);
+// app.post('/user/downloadImage',FBAuth,downloadImage);
+
+exports.api=functions.https.onRequest(app);
